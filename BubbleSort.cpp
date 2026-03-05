7d7e73bcfec2cd9b67a9a919c6541bb9fd3f1439
prinses-racwa <rajwaamelia@gmail.com>
2026-03-05 12:11:25 +0700

    menambahkan proedur buble

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -26,3 +26,28 @@ void input() {
     }
 }
 
+void bubbleSortArray() {
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp;
+                temp = arr [j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr{k} << " ";
+
+        }
+        cout << endl;
+
+    } while (pass <= n - 1);
+}
+
+
